52f56471a860f5bf8d585c646cee5f6733ad1de1
WILLIAM JAMES <161729235+williamjames2004@users.noreply.github.com>
2026-01-21 13:33:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(express.json());
 connectDB();
 
 // Routes
-app.use("/api", authRoutes);
+app.use("/", authRoutes);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
