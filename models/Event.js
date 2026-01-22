b7fea1a2e784fa0ee07f1fc8bbc8baf866dc5eb3
WILLIAM JAMES <161729235+williamjames2004@users.noreply.github.com>
2026-01-22 21:29:42 +0530

    Update Event.js

--- models/Event.js
+++ models/Event.js
@@ -2,15 +2,21 @@ const mongoose = require("mongoose");
 
 const eventSchema = new mongoose.Schema({
   leaderId: { type: String, required: true },
-  name: { type: String, required: true },
-  registerNumber: { type: String, required: true },
+  name: { type: String, default: null },
+  registerNumber: { type: String, default: null },
+  department: {
+    type: String,
+    enum: ["cs", "it", "ai", "ds", "ca"],
+    default: null
+  },
+  college: { type: String, default: null },
   degree: {
     type: String,
     enum: ["ug", "pg"],
-    required: true
+    default: null
   },
-  event1: { type: String },
-  event2: { type: String }
+  event1: { type: String, default: null },
+  event2: { type: String, default: null }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Event", eventSchema);
