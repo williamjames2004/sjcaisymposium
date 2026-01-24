c6b9ed0e127a115ae930819c3a000ad5f6ae7642
Jeeva Loganathan <169042323+jeeva64@users.noreply.github.com>
2026-01-24 16:56:54 +0530

    Add registeredStatus field to CollegeSchema
    registeredStatus field to display only those colleges in the super and moderate admin dashboard

--- models/Colleges.js
+++ models/Colleges.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 
-const CollegeSchema = new mongoose.Schema({
-  collegeId: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  state: { type: String, required: true },
-  district: { type: String, required: true },
-  city: { type: String, required: true }
-}, { timestamps: true });
+const CollegeSchema = new mongoose.Schema(
+  {
+    collegeId: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    state: { type: String, required: true },
+    district: { type: String, required: true },
+    city: { type: String, required: true },
+
+    // ✅ Optional field (NOT required)
+    registeredStatus: {
+      type: Boolean,
+      default: false   // false = 0, true = 1
+    }
+  },
+  { timestamps: true }
+);
+
 
 module.exports = mongoose.model('College', CollegeSchema);
