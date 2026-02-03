181b40c8efefd98f859103cbbfd726630afec171
Jeeva Loganathan <169042323+jeeva64@users.noreply.github.com>
2026-02-03 21:20:54 +0530

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const Admin = require("../models/Admin");
-const Event = require("../models/Event");
 const User = require("../models/User");
 const EventRegistration = require("../models/EventRegistration");
 
@@ -169,5 +168,6 @@ router.post("/vieweventregs", async (req, res) => {
 
 
 module.exports = router;
+
 
 
